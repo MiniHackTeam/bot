984b97cc60a318f906948dafb368da733076ed9e
Polar <polar@polar.blue>
2022-05-17 00:35:40 -0700

    Auto threads.

--- src/bot/events/messageCreate.ts
+++ src/bot/events/messageCreate.ts
@@ -1,3 +1,4 @@
+import { TextChannel } from "discord.js";
 import EventHandler from "../../../lib/classes/EventHandler.js";
 import BetterMessage from "../../../lib/extensions/BetterMessage.js";
 
@@ -6,7 +7,27 @@ export default class MessageCreate extends EventHandler {
         this.client.dataDog.increment("messagesSeen");
         if (message.author.bot) return;
         // @ts-ignore
-        else if (this.client.mongo.topology.s.state === "connected")
-            this.client.textCommandHandler.handleCommand(message);
+        else if (this.client.mongo.topology.s.state !== "connected") return;
+
+        if (this.client.config.threadChannels.includes(message.channel.id)) {
+            const threadId = await this.client.mongo
+                .db("data")
+                .collection("misc")
+                .findOneAndUpdate(
+                    { threadCount: { $exists: true } },
+                    { $inc: { threadCount: 1 } },
+                    { upsert: true, returnDocument: "after" }
+                );
+
+            await (message.channel as TextChannel).threads.create({
+                startMessage: message,
+                autoArchiveDuration: "MAX",
+                reason: "Automatic thread system.",
+                name: `Comments (${threadId.value?.threadCount || 0})`
+            });
+        }
+
+        return this.client.textCommandHandler.handleCommand(message);
     }
 }
+
